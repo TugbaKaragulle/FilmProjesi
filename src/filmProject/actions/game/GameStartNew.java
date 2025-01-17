f6164c89c98ffdc703dc311f5cb49aade5d968ec
Azize Karabakla <azize.karabakla@gmail.com>
2025-01-17 12:15:34 +0100

    Color add

--- src/filmProject/actions/game/GameStartNew.java
+++ src/filmProject/actions/game/GameStartNew.java
@@ -7,6 +7,15 @@ import java.util.Scanner;
 
 public class GameStartNew extends Action {
     private final List<String> films;
+
+    public static final String RESET = "\u001B[0m";
+    public static final String CYAN = "\u001B[36m";
+    public static final String BOLD = "\u001B[1m";
+    public static final String RED = "\u001B[31m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String BLUE = "\u001B[34m";
+
+
     private Scanner scan = new Scanner(System.in);
     int filmNo;
 
@@ -23,13 +32,13 @@ public class GameStartNew extends Action {
     public void hakSayisi(List<String> films, int filmNo) {
         // Seçilen filmin harf sayısını boşlukları hariç hesaplayan method. Replace ile bosluklari hiclikle degistirdik ve dogru harf sayisina ulastik
         String filmAdi = films.get(filmNo - 1).replace(" ", "");
-        System.out.println("Seçtiğiniz film " + filmAdi.length() + " harfli.");
+        System.out.println(BLUE + "Seçtiğiniz film " + filmAdi.length() + " harfli." + RESET);
     }
 
     public void kullanicininFilmNoGirisKontrolu(List<String> films) {
         // Kullanıcıdan geçerli bir film numarası almayi saglayan method.
         // Kullanici harf girerse, 0 , eksi deger ya da listedeki film sayisindan büyük bir rakam girerse bu kontrol devreye girecek
-        System.out.print("Lütfen tahmin için 1 ile " + films.size() + " arasında bir film numarası giriniz: ");
+        System.out.print(BOLD + "Lütfen tahmin için 1 ile " + films.size() + " arasında bir film numarası giriniz: " + RESET);
 
         do {
             if (scan.hasNextInt()) { // Girilen değerin rakam olup olmadığını kontrol ediyoruz
@@ -39,11 +48,11 @@ public class GameStartNew extends Action {
                     hakSayisi(films, filmNo); // filmin harf sayisini ekrana yazdiriyoruz
                     break; // dogru girerse döngü biter
                 } else {
-                    System.out.println("Tahmin için 1 ile " + films.size() + " arasında bir sayı giriniz:"); //Films.size() methoduyla dinamik hale getirdik.
+                    System.out.println(BOLD + "Tahmin için 1 ile " + films.size() + " arasında bir sayı giriniz:" + RESET); //Films.size() methoduyla dinamik hale getirdik.
                     // Admin film ekler ya da cikarirsa kodumuz bundan olumsuz etkilenmeyecek
                 }
             } else {
-                System.out.println("Yalnızca 1 ile " + films.size() + " arasında bir sayı girmelisiniz:");
+                System.out.println(BOLD + "Yalnızca 1 ile " + films.size() + " arasında bir sayı girmelisiniz:" + RESET);
                 scan.nextLine(); // Hatali giriste temizleme yapar
             }
         } while (true); // Kullanicidan dogru girisi alana kadar döngümüz devam eder
@@ -51,7 +60,7 @@ public class GameStartNew extends Action {
 
     public void oyunuOyna(List<String> films) {
 
-        System.out.println("***************OYUN BAŞLIYOR****************");
+        System.out.println(RED + BOLD + "***************OYUN BAŞLIYOR****************" + RESET);
 
         //Kullanicidan kontrollü sekilde tahmin etmek istedigi film nosunu aliyoruz
         kullanicininFilmNoGirisKontrolu(films);
@@ -76,9 +85,9 @@ public class GameStartNew extends Action {
 
         while (kalanHak > 0 && !filmBulundu) { // Kullanicinin tahmin hakki bitene ya da filmi bulana kadar döngü devam eder
             // her seferinde filmin adini, tahmin hakkini ve harf tahmin edin yazisini ekrana getiren kisim:
-            System.out.println("Filmin Adı: " + String.valueOf(gizliFilm)); // karakter arr'yi tekbir stringe cevirir ve dogru bilinen harflerle filmi ekranda gösterir
-            System.out.println("Tahmin hakkınız: " + kalanHak);
-            System.out.print("Bir harf tahmin edin: ");
+            System.out.println(BOLD + "Filmin Adı: " + String.valueOf(gizliFilm) + RESET); // karakter arr'yi tekbir stringe cevirir ve dogru bilinen harflerle filmi ekranda gösterir
+            System.out.println(BOLD + "Tahmin hakkınız: " + kalanHak + RESET);
+            System.out.print(BOLD + "Bir harf tahmin edin: " + RESET);
 
             // Kullanıcıdan harf aliyoruz ve char olarak kaydediyoruz
             char tahmin = scan.nextLine().toLowerCase().charAt(0);
@@ -95,17 +104,17 @@ public class GameStartNew extends Action {
 
             if (dogruTahmin) { // Doğru tahmin durumunda //bayragimiz true iseee
                 dogruTahminSayisi++; // dogru tahmin sayimiz artacak
-                System.out.println("Doğru tahmin!"); //ekrana yazdiracak
+                System.out.println(BLUE + "Doğru tahmin!" + RESET); //ekrana yazdiracak
             } else { // yanlis iseee
                 yanlisTahminSayisi++; // yanlis tahmin sayisi artck
-                System.out.println("Yanlış tahmin!"); //ekrana yazdiracak
+                System.out.println(RED + "Yanlış tahmin!" + RESET); //ekrana yazdiracak
             }
 
             kalanHak--; // kalan hak her seferinde düser
 
             // Kullanici dogru ve yanlis tahmin sayisini görebilecek
-            System.out.println("Doğru tahmin sayısı: " + dogruTahminSayisi);
-            System.out.println("Yanlış tahmin sayısı: " + yanlisTahminSayisi);
+            System.out.println(BOLD + "Doğru tahmin sayısı: " + dogruTahminSayisi + RESET);
+            System.out.println(BOLD + "Yanlış tahmin sayısı: " + yanlisTahminSayisi + RESET);
             System.out.println("                                "); //bu bosluk estetik dursun diye :-)
 
             // char arr stringe dönüsüp de bosluklar kaldirildiginda, bosluklari kaldirilmis film adimizla esit olursa film dogru bilinmis olacak
@@ -116,9 +125,9 @@ public class GameStartNew extends Action {
 
         // Oyunun sonucunu yazdırıyoruz
         if (filmBulundu) { //filmbulundu bayragimiz true olursa
-            System.out.println("Tebrikler! Filmi buldunuz: " + filmAdi.toUpperCase()); //ekrana tabrik mesajimiyi sonuc olarak ekleriz
+            System.out.println(CYAN + "Tebrikler! Filmi buldunuz: " + filmAdi.toUpperCase() + RESET); //ekrana tabrik mesajimiyi sonuc olarak ekleriz
         } else {
-            System.out.println("Tahmin hakkınız maalesef bitti! Film: " + filmAdi.toUpperCase()); //bulunamadiginda ekrana gelecek olan sonuc mesaji
+            System.out.println(GREEN + "Tahmin hakkınız maalesef bitti! Film: " + filmAdi.toUpperCase() + RESET); //bulunamadiginda ekrana gelecek olan sonuc mesaji
         }
     }
 }
